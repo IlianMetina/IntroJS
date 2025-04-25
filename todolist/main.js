288c6debb098e3a6fa5f07ec71b9af2c6b49eb08
Ilian Metina <ilian.metina83@gmail.com>
2025-04-25 14:55:00 +0200

    todolist

--- todolist/main.js
+++ todolist/main.js
@@ -1,30 +1,23 @@
-const addNewTask = document.querySelector(".default-page i");
+const newTaskButton = document.querySelector(".navigation i");
+const newTask = document.querySelector(".navigation");
+const formulaire = document.querySelector(".formulaire");
+const addNewTask = document.querySelector(".add-new-task i");
+
+newTaskButton.addEventListener("click", ()=>{
+
+    newTask.classList.add("passive");
+    formulaire.classList.add("active");
+
+});
+
 addNewTask.addEventListener("click", ()=>{
 
-    const addingTask = document.querySelector(".addingTask");
-    addingTask.classList.add("active");
-    const defaultPage = document.querySelector(".default-page");
-    defaultPage.classList.add("passive");
-
-});
-
-/* .addingTask.active{  .default-page.passive{ */
-
-const addTask = document.querySelector(".ajout");
-addTask.addEventListener("click", ()=>{
-
-    const titreInput = document.querySelector(".title-task");
-    const texteInput = document.querySelector(".text-task");
-    const titre = titreInput.value;
-    const texte = texteInput.value;
-
-    const newTask = document.createElement("div");
-
-    const addingTask = document.querySelector(".addingTask");
-    addingTask.classList.remove("active");
-    const defaultPage = document.querySelector(".default-page");
-    defaultPage.classList.remove("passive");
+    const newBalise = document.createElement("p");
+    newTask.appendChild(newBalise);
+    
 
 });
 
 
+
+/* Réagir à autre chose que le clic pour submit ? */
---
+++ todolist/responsive.md
@@ -0,0 +1,5 @@
+pour écran PC :
+
+- Modifier la classe avec le bouton "add-new-task" pour que le bouton apparaisse moins
+large
+- Balise template comment l'utiliser car dans l'exemple du cours, il y a déjà une instance (?) sauf que je dois le créer de 0 que lorsque il a choisi d'ajouter une tâche.
